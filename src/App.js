805b51e2f8b436e6f68b05abb684efcb8c7d869c
Alan Aliaga <alanaliagadev@gmail.com>
2018-08-17 15:36:50 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <main className="App">
+        <h1>hey</h1>
+      </main>
+    );
+  }
+}
+
+export default App;
